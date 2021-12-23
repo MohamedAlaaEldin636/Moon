58f7dfe729fe632fff6909f896e770e98d4431ed
4msystems400 <4msystems400@gmail.com>
2021-12-23 19:03:21 +0200

    updates

---
+++ presentation/src/main/java/com/structure/base_mvvm/presentation/group_details/GroupDetailsFragment.kt
@@ -0,0 +1,46 @@
+package com.structure.base_mvvm.presentation.group_details
+
+import androidx.fragment.app.viewModels
+import com.structure.base_mvvm.domain.utils.Constants
+import com.structure.base_mvvm.presentation.R
+import com.structure.base_mvvm.presentation.base.BaseFragment
+import com.structure.base_mvvm.presentation.base.extensions.navigateSafe
+import com.structure.base_mvvm.presentation.databinding.FragmentGroupDetailsBinding
+import com.structure.base_mvvm.presentation.databinding.FragmentTeacherProfileBinding
+import com.structure.base_mvvm.presentation.databinding.FragmentTeachersBinding
+import com.structure.base_mvvm.presentation.group_details.viewModels.GroupDetailsViewModel
+import com.structure.base_mvvm.presentation.home.HomeFragmentDirections
+import com.structure.base_mvvm.presentation.teachers.viewModels.TeacherProfileViewModel
+import com.structure.base_mvvm.presentation.teachers.viewModels.TeachersViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class GroupDetailsFragment : BaseFragment<FragmentGroupDetailsBinding>() {
+
+  private val viewModel: GroupDetailsViewModel by viewModels()
+
+  override
+  fun getLayoutId() = R.layout.fragment_group_details
+
+  override
+  fun setBindingVariables() {
+    binding.viewModel = viewModel
+  }
+
+  override
+  fun setUpViews() {
+    setUpToolBar()
+  }
+
+  private fun setUpToolBar() {
+//    binding.includedToolbar.toolbarTitle.text = getMyString(R.string.search)
+//    binding.includedToolbar.backIv.hide()
+  }
+
+  override fun setupObservers() {
+//    viewModel.clickEvent.observeForever {
+//      if (it == Constants.REVIEWS)
+//
+//    }
+  }
+}
---
+++ presentation/src/main/java/com/structure/base_mvvm/presentation/teachers/AskPrivateSessionFragment.kt
@@ -0,0 +1,45 @@
+package com.structure.base_mvvm.presentation.teachers
+
+import androidx.fragment.app.viewModels
+import com.structure.base_mvvm.domain.utils.Constants
+import com.structure.base_mvvm.presentation.R
+import com.structure.base_mvvm.presentation.base.BaseFragment
+import com.structure.base_mvvm.presentation.base.extensions.navigateSafe
+import com.structure.base_mvvm.presentation.databinding.FragmentAskPrivateSessionBinding
+import com.structure.base_mvvm.presentation.teachers.viewModels.AskPrivateSessionViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AskPrivateSessionFragment : BaseFragment<FragmentAskPrivateSessionBinding>() {
+
+  private val viewModel: AskPrivateSessionViewModel by viewModels()
+
+  override
+  fun getLayoutId() = R.layout.fragment_ask_private_session
+
+  override
+  fun setBindingVariables() {
+    binding.viewModel = viewModel
+  }
+
+  override
+  fun setUpViews() {
+    setUpToolBar()
+  }
+
+  private fun setUpToolBar() {
+//    binding.includedToolbar.toolbarTitle.text = getMyString(R.string.search)
+//    binding.includedToolbar.backIv.hide()
+  }
+
+  override fun setupObservers() {
+    viewModel.clickEvent.observeForever {
+      if (it == Constants.REVIEWS)
+        toReviews()
+    }
+  }
+
+  fun toReviews() {
+    navigateSafe(TeacherProfileFragmentDirections.actionToReviewsFragment())
+  }
+}
