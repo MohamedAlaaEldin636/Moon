55c7b40b2fcb316b094ffde3a3134c0fd9f686d6
Mohamed Alaa <mohamed.alaa636@hotmail.com>
2023-02-16 13:46:42 +0200

    Working on explore comments + Splash Speed + Application launch speed and issue

---
+++ app/src/main/java/grand/app/moon/presentation/splash/MASplash2Activity.kt
@@ -0,0 +1,18 @@
+package grand.app.moon.presentation.splash
+
+import android.os.Bundle
+import grand.app.moon.R
+import grand.app.moon.databinding.ActivityMaSplash2Binding
+import grand.app.moon.presentation.base.BaseActivity
+
+class MASplash2Activity : BaseActivity<ActivityMaSplash2Binding>() {
+
+	override fun getLayoutId(): Int = R.layout.activity_ma_splash_2
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+
+
+	}
+
+}
